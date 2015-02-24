6d22a9726596173c688fac9d00cba750ad6d0de0
Jani Anttonen <JaniAnttonen@gmail.com>
2015-02-24 15:30:46 +0200

    Fixed installation problems on remote

--- public/modules/beers/services/breweryDBAPI.client.service.js
+++ public/modules/beers/services/breweryDBAPI.client.service.js
@@ -1,6 +1,9 @@
 'use strict';
 
-// var apiKey = '22e999ac8810c0fd5b09f21344c77873';
+// Connect to Brewery DB
+var BreweryDb = require('brewerydb-node');
 
 // Connection to Brewery DB API
-angular.module('beers').factory('BreweryDBAPI');
+angular.module('beers').factory('BreweryDBAPI',
+
+);
